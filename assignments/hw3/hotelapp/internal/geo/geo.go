ea3f4287aea5aac4d001323df53aef40e917ef23
NikolasTh90 <Nikolasth90@gmail.com>
2022-11-10 16:16:26 +0200

    geo go

--- assignments/hw3/hotelapp/internal/geo/geo.go
+++ assignments/hw3/hotelapp/internal/geo/geo.go
@@ -7,13 +7,9 @@ import (
 	"net"
 	"time"
 
-	"github.com/grpc-ecosystem/grpc-opentracing/go/otgrpc"
 	"github.com/hailocab/go-geoindex"
 	"github.com/opentracing/opentracing-go"
 	pb "github.com/ucy-coast/hotel-app/internal/geo/proto"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/keepalive"
-	"google.golang.org/grpc/reflection"
 )
 
 // Geo implements the geo service
@@ -44,13 +40,57 @@ func NewGeo(a string, p int, db *DatabaseSession, tr opentracing.Tracer) *Geo {
 // Run starts the server
 func (s *Geo) Run() error {
 	// TODO: Implement me
+	if s.port == 0 {
+		return fmt.Errorf("server port must be set")
+	}
+
+	opts := []grpc.ServerOption{
+		grpc.KeepaliveParams(keepalive.ServerParameters{
+			Timeout: 120 * time.Second,
+		}),
+		grpc.KeepaliveEnforcementPolicy(keepalive.EnforcementPolicy{
+			PermitWithoutStream: true,
+		}),
+		grpc.UnaryInterceptor(
+			otgrpc.OpenTracingServerInterceptor(s.tracer),
+		),
+	}
+
+	// Create an instance of the gRPC server
+	srv := grpc.NewServer(opts...)
+
+	// Register our service implementation with the gRPC server
+	pb.RegisterGeoServer(srv, s)
+
+	// Register reflection service on gRPC server.
+	reflection.Register(srv)
+
+	// Listen for client requests
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", s.port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	// Accept and serve incoming client requests
+	log.Printf("Start Profile server. Addr: %s:%d\n", s.addr, s.port)
+	return srv.Serve(lis)
 }
 
 // Nearby returns all hotels within a given distance.
 func (s *Geo) Nearby(ctx context.Context, req *pb.Request) (*pb.Result, error) {
 	// TODO: Implement me
-	// HINT: Reuse the implementation from the monolithic implementation 
+	// HINT: Reuse the implementation from the monolithic implementation
 	// HINT: and modify as needed.
+	var (
+		points = s.getNearbyPoints(float64(req.Lat), float64(req.Lon))
+	)
+	res := new(pb.Result)
+
+	for _, p := range points {
+		res.HotelIds = append(res.HotelIds, p.Id())
+	}
+
+	return res, nil
 }
 
 func (s *Geo) getNearbyPoints(lat, lon float64) []geoindex.Point {
