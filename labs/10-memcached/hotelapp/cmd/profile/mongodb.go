7a391d8d469b316914291f2b160b9c7040e6de62
NikolasTh90 <Nikolasth90@gmail.com>
2022-11-22 15:33:35 +0200

    lab 10

--- labs/10-memcached/hotelapp/cmd/profile/mongodb.go
+++ labs/10-memcached/hotelapp/cmd/profile/mongodb.go
@@ -3,6 +3,7 @@
 package main
 
 import (
+	"time",
 	"flag"
 	"path/filepath"
 
@@ -10,11 +11,11 @@ import (
 )
 
 var (
+	memcache_addr = flag.String("memc_addr", "memcached-profile:11211", "Address of the memcache server")
 	database_addr = flag.String("db_addr", "mongodb-profile:27017", "Address of the data base server")
 )
 
 func initializeProfileDatabase() *profile.DatabaseSession {
-	dbsession := profile.NewDatabaseSession(*database_addr)
-	dbsession.LoadDataFromJsonFile(filepath.Join(*jsonDataDir, "hotels.json"))
+	dbsession := profile.NewDatabaseSession(*database_addr, *memcache_addr)	dbsession.LoadDataFromJsonFile(filepath.Join(*jsonDataDir, "hotels.json"))
 	return dbsession
 }
